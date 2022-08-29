e976e2f8e275c03483152cddee64087d66bf1fdc
zeljko <zeljko@bonfire.com>
2022-08-29 13:14:31 +0200

    95cde5e updated image

---
+++ scripts/google-analytics.js
@@ -0,0 +1,16 @@
+// Set to the same value as the web property used on the site
+var gaProperty = 'UA-52338453-1';
+
+// Disable tracking if the opt-out cookie exists.
+var disableStr = 'ga-disable-' + gaProperty;
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+	window[disableStr] = true;
+}
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52338453-1', 'auto');
+  ga('send', 'pageview');
